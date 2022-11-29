3249c8961d3f689c09fa37f3360c9782fa356322
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-11-29 17:03:52 +0530

    login completed

--- login.js
+++ login.js
@@ -1,2 +1,12 @@
 let regEmail = 'gaganbn1999@gmail.com';
 let pass = '1234';
+
+
+let entEmail = 'gaganbn1999@gmail.com';
+let entpass = '1234';
+
+if(regEmail == entEmail && pass == entpass) {
+console.log('You can login');
+} else {
+console.log('You can not login');
+}
